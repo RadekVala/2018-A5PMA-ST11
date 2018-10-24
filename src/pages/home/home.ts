5167e419afb19c8207973d050e5040bc7bf5e2b3
Student <student@iMac-B202-00.local>
2018-10-24 12:47:49 +0200

    Text to speech plugin ADDED

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { TranslationProvider } from '../../providers/translation/translation';
 import { HistoryProvider } from '../../providers/history/history';
+import { TextToSpeech } from '@ionic-native/text-to-speech';
 
 @Component({
   selector: 'page-home',
@@ -15,7 +16,8 @@ export class HomePage {
   constructor(
     public navCtrl: NavController,
     private translationProvider: TranslationProvider,
-    private historyProvider: HistoryProvider
+    private historyProvider: HistoryProvider,
+    private tts: TextToSpeech
   ) {
 
   }
@@ -23,7 +25,6 @@ export class HomePage {
   // click event handler
   public btnTranslateClicked(userInput:string):void {
     console.log(userInput);
-    
     this.userInput = userInput;
     this.translationProvider.getTranslationResult(userInput).subscribe(
       (response) => {
@@ -31,6 +32,10 @@ export class HomePage {
         console.log(response);
         
         this.result = response.responseData.translatedText;
+
+        this.tts.speak(this.result)
+        .then(() => console.log('Success'))
+        .catch((reason: any) => console.log(reason));
 
         this.historyProvider.saveToHistory(userInput, this.result);
       }
